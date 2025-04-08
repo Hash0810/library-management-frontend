9bb58bfcdf4d9342f028b7e06d288e23d387742e
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-08 10:49:31 +0530

    Update IssueBook.js

--- src/pages/IssueBook.js
+++ src/pages/IssueBook.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../api";
-import "./IssueBook.css";
+import "../styles/IssueBook.css";
 
 function BookRequestManager() {
   const [requests, setRequests] = useState([]);
